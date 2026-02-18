5ee91023bd66bed0da2e5744d8887c45462d3023
Luca Fresta <lucafresta@Lucas-MacBook-Pro.local>
2026-02-18 14:59:04 +0100

    Update header layout and boxes

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,15 @@ REMOTE_PATH=/www/public/users/pmp-seminars/
 rsync -avz --delete $LOCAL_REPO $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 
 ssh $REMOTE_USER@$REMOTE_HOST "chmod 644 $REMOTE_PATH*; chmod 755 $REMOTE_PATHimages"
+
+ssh "$REMOTE_USER@$REMOTE_HOST" << 'ENDSSH'
+cd /www/public/users/pmp-seminars
+
+# Make all files readable
+find /www/public/users/pmp-seminars -type f -exec chmod 644 {} \;
+
+# Make all directories accessible
+find /www/public/users/pmp-seminars -type d -exec chmod 755 {} \;
+ENDSSH
+
+echo "Deploy completed. All files and images are now readable."
